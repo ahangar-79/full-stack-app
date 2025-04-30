2491e491a5c18e5a56fe0b08eb073c7e2a3f2c37
ahangar <ahangar6169@gmail.com>
2025-04-30 18:36:01 +0330

    build navbar, login, logout, fix auth and route file

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,38 +2,56 @@ import { auth, signIn, signOut } from "@/auth";
 import Image from "next/image";
 import Link from "next/link";
 
-export default async function Navbar() {
-const session = await auth()
+const Navbar = async () => {
+  const session = await auth();
+  console.log(session)
 
   return (
-    <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
-      <nav className="flex justify-between items-center">
-        <Link href="/">
-          <Image src="/logo.png" alt="logo" width={144} height={30} />
-        </Link>
-        
-        <div className="flex items-center gap-5 text-black">
+    <div>
+      <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
+        <nav className="flex justify-between items-center">
+          <Link href="/">
+            <Image src="/logo.png" alt="logo" width={144} height={30} />
+          </Link>
+
+          <div className="flex items-center gap-5 text-black">
             {session && session?.user ? (
-                <>
-                <Link href='/startup/create'>
-                   <span>Create</span>
+              <>
+                <Link href="/startup/create">
+                  <span>Create</span>
                 </Link>
 
-                <button onClick={signOut}>
+                <form
+                  action={async () => {
+                    "use server";
+                    await signOut();
+                  }}
+                >
+                  <button className="cursor-pointer" type="submit">
                     <span>Logout</span>
-                </button>
+                  </button>
+                </form>
 
-                <Link href={`/user/${session?.id}`}>
-                    <span>{session?.user?.name}</span>
+                <Link href={`/user/${session?.user?.name}`}>
+                  <span>{session?.user?.name}</span>
                 </Link>
-                </>
-            ):(
-               <button onClick={signIn( provider: 'github')}>
-                    <span>Login</span>
-               </button>
+              </>
+            ) : (
+              <form
+                action={async () => {
+                  "use server";
+
+                  await signIn("github");
+                }}
+              >
+                <button type="submit">Login</button>
+              </form>
             )}
-        </div>
-      </nav>
-    </header>
-  )
-}
+          </div>
+        </nav>
+      </header>
+    </div>
+  );
+};
+
+export default Navbar;
