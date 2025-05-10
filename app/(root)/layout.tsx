a23d1485003833a76198b058f6bda891f1053a50
ahangar <ahangar6169@gmail.com>
2025-05-10 17:55:09 +0330

    add form, reset and submit button and some change

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,13 @@
 import React from "react";
-import Navbar from "../components/Navbar";
+import Navbar from "../../components/ui/Navbar";
 
-export default function Layout({children}: Readonly<{children: React.ReactNode}>) {
-    return(
-        <main className="font-work-sans">
-            <Navbar/>
-            {children}
-        </main>
-    )
+export default function Layout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <main className="font-work-sans">
+      <Navbar />
+      {children}
+    </main>
+  );
 }
