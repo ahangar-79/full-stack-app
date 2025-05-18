3ad2d23447cffb94fc52566b2425f8e0af215f1a
ahangar <ahangar6169@gmail.com>
2025-05-18 18:31:37 +0330

    add schema and sanity-typegen and some other change

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,11 @@
-// import { describe } from "node:test";
 import SearchForm from "../../components/ui/SearchForm";
+import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
+import { STARTUPS_QUERY } from "@/sanity/lib/queries";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
+// import { describe } from "node:test";
+// import { client } from "@/sanity/lib/client";
 // import { startTurbopackTraceServer } from "next/dist/build/swc/generated-native";
-import StartupCard from "@/components/StartupCard";
+
 
 export default async function Home({
   searchParams,
@@ -9,19 +13,23 @@ export default async function Home({
   searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query;
+  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY });
 
-  const posts =[{
-    _createAt: new Date(),
-    views: 55,
-    author: { _id: 1, name:'adrian'},
-    _id: 1,
-    description: 'This is description',
-    // image: 'https://placehold.co/600*400',
-    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_7QZevMIc8mNU6wYV8O1xBgcNHlHDvNGh2A&s',
-    // image: 'https://espacio.fundaciontelefonica.com/audioguias/en/wp-content/uploads/sites/2/2018/10/NOSOTROS-ROBOTS.jpg',
-    category: 'Robots',
-    title: 'We robots'
-  }]
+  // const posts = await client.fetch(STARTUPS_QUERY);
+  // console.log(JSON.stringify(posts));
+
+  // const posts =[{
+  //   _createAt: new Date(),
+  //   views: 55,
+  //   author: { _id: 1, name:'adrian'},
+  //   _id: 1,
+  //   description: 'This is description',
+  //   // image: 'https://placehold.co/600*400',
+  //   image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_7QZevMIc8mNU6wYV8O1xBgcNHlHDvNGh2A&s',
+  //   // image: 'https://espacio.fundaciontelefonica.com/audioguias/en/wp-content/uploads/sites/2/2018/10/NOSOTROS-ROBOTS.jpg',
+  //   category: 'Robots',
+  //   title: 'We robots'
+  // }]
 
   return (
     <>
@@ -42,13 +50,17 @@ export default async function Home({
         </p>
 
         <ul className="mt-7 card_grid">
-          {posts ?.length > 0 ? (posts.map((post) =>(
-            <StartupCard key={post?._id} post={post}/>
-          ))) : (
+          {posts?.length > 0 ? (
+            posts.map((post: StartupTypeCard) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
             <p className="no-results">No startups found</p>
           )}
         </ul>
       </section>
+
+      <SanityLive />
     </>
   );
 }
